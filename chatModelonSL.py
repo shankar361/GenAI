8fa406ec64c5d99e96dd104f0a123ea7d821e953
Shankar Bhagat <40621440+shankar361@users.noreply.github.com>
2025-04-24 09:35:16 +0530

    Update chatModelonSL.py

--- chatModelonSL.py
+++ chatModelonSL.py
@@ -1,10 +1,11 @@
 from docx import Document as DocxDocument
+import speech_recognition as SR
 import streamlit as st
 from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.chains.retrieval_qa.base import RetrievalQA
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+#from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyMuPDFLoader
 from pdf2docx import Converter
 from langchain.schema import Document
@@ -20,12 +21,20 @@ if "intro" not in st.session_state:
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = ""
 
+if "singleConv" not in st.session_state:
+    st.session_state.singleConv = []
+
 if "query_input" not in st.session_state:
     st.session_state.query_input = ""
+
+if "prevQuery" not in st.session_state:
+    st.session_state.prevQuery=""
 
 if "query" not in st.session_state:
     st.session_state.query = ""
 
+if "chat_query" not in st.session_state:
+    st.session_state.chat_query=""
 if "docs" not in st.session_state:
     st.session_state.docs = []
 
@@ -50,8 +59,9 @@ else:
         base_url  = "https://api.groq.com/openai/v1",
         api_key   = api_key,
       # model_name= "llama3-70b-8192"  #model name may reach limit
-     #  model_name= "llama3-8b-8192"
-        model_name= "llama3-70b-8192"
+    #    model_name= "llama3-8b-8192"  # reached limit
+        model_name="llama-3.1-8b-instant"
+     #  model_name= "llama3-70b-8192"  #reached limit
     )   
         
 def clear_text():
@@ -60,29 +70,33 @@ def clear_text():
     st.session_state.query_input = ""
 
 def buildResult(response):
-                    st.write("You : "+response["query"])   
-                    st.write("HRBuddy : "+response["result"])   
-                    #put/append into chat box                                                 
-                    st.session_state.chat_history +="\tYou:\n"+response["query"] + "\n"
-                    line = '_' * 80               
-                    st.session_state.chat_history +="\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tHRBuddy:\n"
-                    st.session_state.chat_history +=response["result"]+"\n"                  
-                    st.session_state.chat_history+=line + "\n"
-                    # st.text_area("Conversation with HRBuddy", value=st.session_state.chat_history, height=400)
-                    st.write("Full Conversation with HRBuddy (Download from left panel):")
-                    render_chat(st.session_state.chat_history)
+    st.write("🧑 You : "+response["query"])   
+    st.write("🤖 HRBuddy : "+response["result"]) 
+    q = "\n"
+    q += response["query"]
+    r  = response["result"]                                                       
+    line = '_' * 80                    
+    # st.text_area("Conversation with HRBuddy", value=st.session_state.chat_history, height=400)
+    st.write("Full Conversation with HRBuddy (Download from left panel):")
+    r +="\n"+ line
+    st.session_state.singleConv.append((q,r))           
+    render_chat(st.session_state.singleConv)
 
 def CheckIfResumeUploaded(qa):
  if st.session_state.resume_verified == False:
-        resp = qa.invoke("Is this a resume or is resume uploaded, answer in yes or no.")
-        ans = resp["result"].lower()
-        st.session_state.resume_verified = True
-        if "yes" not in ans:
-            st.error("The candidate's resume is not uploaded! Please upload resume/CV")         
-            st.session_state.resume_verified = False 
-            return False
-        else:
-            return True
+    resp = qa.invoke(
+    """Analyze the uploaded documents. Based on its structure and content,"
+       is this likely a resume or CV of a person?
+       Respond with only 'Yes' or 'No'."""
+)
+    ans = resp["result"].lower()
+    st.session_state.resume_verified = True
+    if "yes" not in ans:
+        st.error("The candidate's resume is not uploaded! Please upload resume/CV")         
+        st.session_state.resume_verified = False 
+        return False
+    else:
+        return True
         
 def start_chat(qa,resumeUploaded):
     if  st.session_state.clear_chat_clicked :
@@ -90,23 +104,30 @@ def start_chat(qa,resumeUploaded):
    
     if resumeUploaded:
         query = st.session_state.query
+        if query:
+            query = query[0].upper() + query[1:]
        # query = st.session_state.query_input.strip()
         upQuery = query.upper()
         if upQuery == "Q" or upQuery == "QUIT" or upQuery == "END" or upQuery == "BYE" :
             st.write("Thank you for using our AI system, Good bye..!")
         else:
-            if st.session_state.query != "" :
-                with st.spinner("Analyzing data based on your query.."):
-                    response = qa.invoke(query)
-                    #if not response["source_documents"]:
+            if query != "" :
+                if query != st.session_state.prevQuery:
+                    with st.spinner("Analyzing data based on your query.."):
+                        response = qa.invoke(query)
+                       # st.write("Source docs:", response["source_documents"])
+                        #if not response["source_documents"]:
                         #st.write("The Question is out of context or the correct documents not provided")
-                    #else:
-                    buildResult(response)
-                 
-                    st.session_state.query=""
-                           
+                        #else:
+                        buildResult(response)
+                        st.session_state.prevQuery = query
+                    #  st.session_state.query = ""
+                else:
+                    render_chat(st.session_state.singleConv)
+                  
+            
 
-def render_chat(chat_history: str):
+def render_chat(singleConv: str):
     st.markdown("""
     <style>
     .chat-box {
@@ -116,54 +137,102 @@ def render_chat(chat_history: str):
         border    : 1px solid #444;
         border-radius: 5px;    
     }
+    .chat-message-user {
+    color: #1f77b4;
+    margin-bottom: 8px;
+    }
+    .chat-message-bot {
+    color: #2ca02c;
+    margin-bottom: 12px;
+    }
     </style>
-    """, unsafe_allow_html=True)
+    """, unsafe_allow_html = True)
 
-    st.markdown(f"<div class='chat-box' id='chatbox'>{chat_history}</div>", unsafe_allow_html=True)
+    chat_html = "<div class='chat-box'>"
+    for query,response in singleConv:
+        chat_html += f"<div class='chat-message-user'>🧑  You: {query}</div>"
+        chat_html += f"<div class='chat-message-bot'>🤖  HRBuddy: {response}</div>"
+    chat_html += "</div>"
+    st.markdown(chat_html, unsafe_allow_html = True)
 
 
-def load_file(file_path):
+
+def load_file(file_path,source_name):
     docx  = DocxDocument(file_path)
+   
     text  = "\n".join([p.text for p in docx.paragraphs if p.text.strip()])
-    return [Document(page_content=text)]
+    tables_text = extractTableData(file_path)
+    fullText = text +"\n"+ tables_text  
+    return [Document(page_content=fullText,metadata={"source": source_name})]
 
-def splitData(pdfData):
-   splitter = RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=100)
-   splittedpdfDoc = splitter.split_documents(pdfData)
-   return splittedpdfDoc
+#def splitData(pdfData):
+#   splitter = RecursiveCharacterTextSplitter(chunk_size = 1000,chunk_overlap=150)
+#   splittedpdfDoc = splitter.split_documents(pdfData)
+#   return splittedpdfDoc
+
+def extractTableData(file_path):
+    tables_text = []
+
+    docx2=DocxDocument(file_path)
+    for tables in docx2.tables:
+        for rows in tables.rows:
+           # for cells in rows.cells:
+            row_data = [cell.text.strip() for cell in rows.cells]
+            tables_text.append(" | ".join(row_data))
+        tables_text.append("-"*50)
+
+    return "\n".join(tables_text)
 
 
 def loadAndSplit_pdfFile(files): 
     loader  = PyMuPDFLoader(files)
     pdfData = loader.load()
-    splittedpdfDoc = splitData(pdfData)
-    return splittedpdfDoc
+  #  splittedpdfDoc = splitData(pdfData)
+    return pdfData
 
 def createTempfile(files):
     with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
         tmp_file.write(files.read())
         return tmp_file.name
+    
+def inputFromMicrophone():
+    recog = SR.Recognizer()
+    with SR.Microphone() as source:
+        with st.spinner("Listening..."):
+            recog.adjust_for_ambient_noise(source)
+            audio = recog.listen(source,timeout=10,phrase_time_limit=10)
+    try:
+        st.session_state.query = recog.recognize_google(audio)
+    except SR.UnknownValueError:
+        st.warning("Could not understand audio,use text box or try again")
+    except SR.RequestError as e:    
+        st.warning("No results for Google Speech Recognition")
+
 
 def  createRetrieverQA(docs,emb):
     vectorStores = FAISS.from_documents(docs,emb)
+    vectorStores.save_local("faiss")
     retriever = vectorStores.as_retriever()
     qa = RetrievalQA.from_llm(
     llm = st.session_state.llm,
     retriever = retriever,
-    return_source_documents=False       
+    return_source_documents=True       
     )
     return qa
 
 
 if "emb" not in st.session_state:
-    st.session_state.emb= HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-
+   # st.session_state.emb= HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    st.session_state.emb= HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 st.title("OnBoarding Pro")
 st.write("Dear HR, How can I help you today?")
 introText = "This is an HR assistance tool which works based on Candidates' profile" \
 "  and interview feedbacs. Please uploaded all supporting document to have best results"
 
-#st.header("******Your Personalized Hiring assistant******")
+with st.sidebar:
+    colImg1,colImg2,colImg13=st.columns(3)
+    with colImg2:
+        st.image("silicon.png")
 path = st.sidebar.file_uploader("Upload Candidate's Resume and supporting docs(*.pdf, *.docx)",
 accept_multiple_files = True,
 help = "Upload one more more files"
@@ -171,27 +240,12 @@ help = "Upload one more more files"
 if st.session_state.intro and path ==[]:
     st.write(introText)
     st.session_state.intro = False
-
-st.text_input("Enter your query here", key="query_input", on_change=clear_text)
-
-if st.sidebar.button("Clear Chat"):
-    st.text_area("Conversation with HRBuddy", value="", height=400)
-    st.session_state.clear_chat_clicked = True
-    st.session_state.chat_history = ""                     
-else:          
-    st.session_state.clear_chat_clicked = False
-    
-#st.write(st.session_state.chat_history)  #debug
-if  st.session_state.chat_history:
-    textBytes = st.session_state.chat_history.encode('utf-8')
-  #  st.write(textBytes)
-    now1 = datetime.now()
-    st.sidebar.download_button(label = "Download Chat",
-    data      = textBytes,
-    file_name = "downloadedChat_"+ now1.strftime("%Y-%m-%d-%H:%M:%S")+".txt",
-    mime      = "text/plain"
-    )
-    
+col1, col2 = st.columns([5, 1])
+with col1:
+    st.text_input("Enter your query here", key="query_input", on_change=clear_text)
+with col2:
+    if st.button("🎤"):
+        inputFromMicrophone()
 
 new_docs = []
 if path is not None:  
@@ -199,22 +253,21 @@ if path is not None:
         tmp = createTempfile(files)
         if files.name.endswith(".pdf"):     
            
-#convert pdf to docx because model works best with docx
             docxTemp = tempfile.NamedTemporaryFile(delete=False, suffix=".docx")
             docxPath = docxTemp.name
             cv = Converter(tmp)
             cv.convert(docxPath,0,end=None)   
         
           #  splittedpdfDoc = loadAndSplit_pdfFile(tmp2)
-            new_docs.extend(load_file(docxPath))
+            new_docs.extend(load_file(docxPath,files.name))
         else:
-            new_docs.extend(load_file(tmp))
+            new_docs.extend(load_file(tmp,files.name))
 
 files_uploaded = [f.name for f in path]if path else []
 files_changed  = (files_uploaded!= st.session_state.uploaded_files)
 
 if len(new_docs) > 0 and files_changed:
-    st.session_state.query = ""
+   # st.session_state.query = ""
     st.session_state.uploaded_files=files_uploaded
     st.session_state.docs = new_docs
     st.session_state.resume_verified = False   
@@ -224,6 +277,32 @@ if len(new_docs) > 0 and files_changed:
 
 if st.session_state.qa: 
     start_chat(st.session_state.qa,st.session_state.resumeUploaded)
-   
+
+with st.sidebar:
+    col3,col4=st.columns(2)    
+    with col3:
+        if st.button("Clear Chat"):
+            st.text_area("Conversation with HRBuddy", value="", height=400)
+            st.session_state.clear_chat_clicked = True
+            st.session_state.singleConv = []                     
+        else:          
+            st.session_state.clear_chat_clicked = False
+            
+
+    with col4:
+        textBytes=bytes()
+        if  st.session_state.singleConv != []:
+            for q,r in st.session_state.singleConv:
+                chat_line = f"You: {q}\nHRBuddy: {r}\n{'_' * 80}\n\n"
+                textBytes += chat_line.encode('utf-8')
+            
+            now1 = datetime.now()
+            st.download_button(label = "Download Chat",
+            data      = textBytes,
+            file_name = "downloadedChat_"+ now1.strftime("%Y-%m-%d-%H:%M:%S")+".txt",
+            mime      = "text/plain"
+            )
+        # st.session_state.singleConv = []  
+  
 if not path:
      st.info("Upload a resume or other supporting documents to begin")
